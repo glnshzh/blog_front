c9b51a5f9f631b4be4b5bb09a61a352e9033d256
glnshzh <478184350@qq.com>
2024-03-23 18:08:47 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import aa from './components/try.vue'
+import loginUser from './components/login.vue'
+import Home from './components/Home.vue';
+</script>
+
+<template>
+    <Home/>
+</template>
---
+++ src/components/try.vue
@@ -0,0 +1,21 @@
+
+<template>
+<button @click="addCount">123</button>
+<P>{{ count }}</P>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            count:0
+        }
+    },
+    methods:{
+        addCount(){
+            this.count+=1
+        }
+    }
+}
+
+</script>
